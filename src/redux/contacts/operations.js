8abf8563c677230a2179057fe0976e961874cf61
keraberas <46618649+keraberase@users.noreply.github.com>
2024-05-05 12:53:06 +0300

    add components

---
+++ src/redux/contacts/operations.js
@@ -0,0 +1,42 @@
+import axios from "axios";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+export const instance = axios.create({
+  baseURL: "https://connections-api.herokuapp.com/",
+});
+
+export const fetchContacts = createAsyncThunk(
+  "contacts/fetchAll",
+  async (_, thunkAPI) => {
+    try {
+      const { data } = await instance.get("/contacts");
+      return data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const addContact = createAsyncThunk(
+  "contacts/addContact",
+  async (newContact, thunkAPI) => {
+    try {
+      const { data } = await instance.post("/contacts", newContact);
+      return data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const deleteContact = createAsyncThunk(
+  "contacts/deleteContact",
+  async (contactId, thunkAPI) => {
+    try {
+      const { data } = await instance.delete(`/contacts/${contactId}`);
+      return data.id;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
